6b8125c872ff8c981745b76d937fb9201fad8b83
Randercrop <austiwang@gmail.com>
2018-11-12 20:19:37 -0800

    get code working

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from flask import (
     redirect,
     jsonify,
     url_for,
-    flash
+    flash,
     session as login_session
 )
 import random
@@ -173,9 +173,8 @@ def gdisconnect():
             json.dumps('Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s'
-    % login_session['access_token']
-    print url
+    url = 'https://accounts.google.com/o/oauth2/revoke?token='
+    url += login_session['access_token']
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
     print('result is ')
